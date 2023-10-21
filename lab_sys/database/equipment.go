7aa5a87b2949bf7f572f6f21fe2bc05fc3db0462
Khalil996 <2649475267@qq.com>
2023-10-22 02:05:50 +0800

    器材表的crud

--- lab_sys/database/equipment.go
+++ lab_sys/database/equipment.go
@@ -1,1 +1,52 @@
 package database
+
+import (
+	"errors"
+	"go.uber.org/zap"
+	"lab_sys/lab_sys/global"
+	"lab_sys/lab_sys/model"
+)
+
+// 获取器材列表
+func GetEquipmentList() ([]*model.Equipment, error) {
+	var list []*model.Equipment
+	if tx := global.DB.Find(&list); tx.RowsAffected == 0 {
+		return nil, errors.New("get equipment list failed")
+	}
+	return list, nil
+}
+
+// 创建器材
+func CreateEquipment(equip model.Equipment) (*model.Equipment, error) {
+	tx := global.DB.Create(&equip)
+	if tx.RowsAffected == 0 {
+		zap.S().Info("create failed")
+		return nil, errors.New("create failed")
+	}
+	return &equip, nil
+}
+
+// 更新器材
+func UpdateEquipment(equip model.Equipment) (*model.Equipment, error) {
+	tx := global.DB.Updates(model.Equipment{
+
+		Name:  equip.Name,
+		Type:  equip.Type,
+		Stock: equip.Stock,
+	})
+	if tx.RowsAffected == 0 {
+		zap.S().Info("update failed")
+		return nil, errors.New("update failed")
+	}
+	return &equip, nil
+}
+
+// 删除器材
+func DeleteEquip(equip model.Equipment) error {
+	tx := global.DB.Delete(&equip)
+	if tx.RowsAffected == 0 {
+		zap.S().Info("delete failed")
+		return errors.New("delete failed")
+	}
+	return nil
+}
